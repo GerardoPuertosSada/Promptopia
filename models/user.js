16235c2510f1ab9def1f72f00f27f30bd979ff26
GerardoPuertosSada <gerardopsada@gmail.com>
2023-06-19 18:20:46 -0600

    creating users and user database

--- models/user.js
+++ models/user.js
@@ -2,8 +2,16 @@ import { Schema, model, models } from 'mongoose'
 
 const UserSchema =  newSchema({
   email: {
-    type: string,
-
-    required: true,
+    type: String,
+    unique: [true, 'Email already exists'],
+    required: [true, 'Email is requiered!']
+  },
+  username: {
+    type: String,
+    required: [true, 'Username is requiered!'],
+    match: [/^(?=.{8,20}$)(?![_.])(?!.*[_.]{2})[a-zA-Z0-9._]+(?<![_.])$/, "Username invalid, it should contain 8-20 alphanumeric letters and be unique!"]
+  },
+  image: {
+    type: String
   }
 })
